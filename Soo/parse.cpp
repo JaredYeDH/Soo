ceb18cb50278f18792da0d1de72c490e0e4d397c
Leviathan <Leviathan1995@outlook.com>
2015-09-25 07:21:53 +0600

    Update parse.cpp

--- Soo/parse.cpp
+++ Soo/parse.cpp
@@ -105,12 +105,22 @@ int parse::binary_lsy()
 		int number1 = num.top();
 		num.pop();
 		string operand = op.top();
+		op.pop();
 		int out = operation(number1, number2, operand);
 		num.push(out);
 	}
 	return num.top();
 }
 
+int parse::getvalue(string name)
+{
+	int value;
+	if (judge_variable(name))
+		value = symbol_getvalue(name);
+	else
+		value = string2int(name);
+	return value;
+}
 //build syntax tree
 void parse::build_ast()
 {
@@ -138,10 +148,9 @@ void parse::build_ast()
 			parse_func();
 		}
 		else if (codestream[index] == "\n")
-			index++;
+			continue;
 	}
 }
-
 //statement or define variable
 void parse::parse_defvar()
 {
@@ -149,32 +158,71 @@ void parse::parse_defvar()
 	index++;
 	string name = codestream[index];
 	index++;
-	//define
-	if (codestream[index] == "=")
+	//array
+	if (codestream[index] == "[")
 	{
 		index++;
-		int number = binary_lsy();
-		number_ast *value = new number_ast(number);
-		defvariable_ast *variable = new defvariable_ast(name, value);
-		variable->codegen();
+		int size = string2int(codestream[index]);
+		index++;
+		// token "]"
+		index++;
+		//define
+		if (codestream[index] == "=")
+		{
+			index++;
+			if (codestream[index] == "{")
+			{
+				index++;
+				vector<int> num;
+				while (codestream[index] != "}")
+				{
+					if (codestream[index] == ",")
+					{
+						index++;
+						continue;
+					}
+					else
+						num.push_back(string2int(codestream[index]));
+					index++;
+				}
+				symbol_insert_intarray(name, num);
+				array_ast * array = new array_ast(name,num,size);
+				array->codegen();
+			}
+		}
+		
 	}
-	//statement
+	// normal variable
 	else
 	{
-		number_ast *value = new number_ast(0);
-		defvariable_ast *variable = new defvariable_ast(name, value);
-		variable->codegen();
+		//define
+		if (codestream[index] == "=")
+		{
+			index++;
+			int number = binary_lsy();
+			symbol_insert_intvalue(name, number);
+			number_ast *value = new number_ast(number);
+			defvariable_ast *variable = new defvariable_ast(name, value);
+			variable->codegen();
+		}
+		//statement
+		else
+		{
+			symbol_insert_intvalue(name, 0);
+			number_ast *value = new number_ast(0);
+			defvariable_ast *variable = new defvariable_ast(name, value);
+			variable->codegen();
+		}
 	}
 }
-
 //binary expression
 void parse::parse_binary(string variable_name)
 {
 	int number = binary_lsy();
 	number_ast *value = new number_ast(number);
 	defvariable_ast * variable = new defvariable_ast(variable_name, value);
+	variable->codegen();
 }
-
 //function
 void parse::parse_func()
 {
@@ -198,9 +246,18 @@ void parse::parse_expreesion()
 			index++;
 			parse_if();
 		}
+		else if (codestream[index] == "while")
+		{
+			index++;
+			parse_while();
+		}
 		else if (judge_variable(codestream[index]))
 		{
-			parse_binary(codestream[index]);
+			string variable_name = codestream[index];
+			while (codestream[index] != "=")
+				index++;
+			index++;
+			parse_binary(variable_name);
 		}
 		//statement or define variable
 		else if (codestream[index] == "var")
@@ -213,28 +270,95 @@ void parse::parse_expreesion()
 	}
 }
 
-void parse::parse_judge()
+void parse::parse_judge(exp_type type)
 {
 	if (codestream[index]=="(")
 	{
 		index++;
-		string left = codestream[index];
-		int left_number = string2int(left);
-		number_ast *left_ast = new number_ast(left_number);
-		index++;
-		string op = codestream[index];
-		index++;
-		string right = codestream[index];
-		int right_number = string2int(right);
-		number_ast *right_ast = new number_ast(right_number);
-		if_ast * judge = new if_ast(op, left_ast, right_ast);
-		judge->codegen();
+		vector<string> codes;
+		while (codestream[index] != ")")
+		{
+			codes.push_back(codestream[index]);
+			index++;
+		}
+		if (type == if_exp)
+		{
+			parse_bool(codes,"next");
+		}
+		else if (type == while_exp)
+		{
+			parse_bool(codes,"next");
+		}
 	}
 	index++; //")"
+}
+//parse bool expression
+void parse::parse_bool(vector<string> codes,string choice)
+{
+	string left = codes[0];
+	int left_number = getvalue(left);
+	immediate_ast *left_ast = new immediate_ast(left_number);
+	string op = codes[1];
+	string right = codes[0];
+	int right_number = getvalue(right);
+	immediate_ast *right_ast = new immediate_ast(right_number);
+	judge_ast *judge = new judge_ast(op, left_ast, right_ast,choice);
+	judge->codegen();
 }
 
 void parse::parse_if()
 {
-	parse_judge();
+	parse_judge(if_exp);
+	while (codestream[index] != "{")
+		index++;
+	index++;
+	/*
+		write label address
+	*/
+	sign_ast * addr_sign = new sign_ast("now");
+	addr_sign->get_sign();
+	addr_sign->codegen();
+	/*
+		block expreesion
+	*/
 	parse_expreesion();
+	/*
+		end label 
+	*/
+	end_ast * end = new end_ast("end");
+	end->codegen();
+}
+
+void parse::parse_while()
+{
+	vector<string> codes;
+	if (codestream[index] == "(")
+	{
+		int while_index = index+1;
+		while (codestream[while_index] != ")")
+		{
+			codes.push_back(codestream[while_index]);
+			while_index++;
+		}
+	}
+	parse_judge(while_exp);
+	while (codestream[index] != "{")
+		index++;
+	index++;
+	/*
+		write label address
+	*/
+	sign_ast * addr_sign = new sign_ast("now");
+	addr_sign->get_sign();
+	addr_sign->codegen();
+	/*
+		block expreesion
+	*/
+	parse_expreesion();
+	/*
+		end label
+	*/
+	end_ast * end = new end_ast("end");
+	end->codegen();
+	parse_bool(codes, "now");
 }
