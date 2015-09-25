a76fe8e7294e1d19949f989f3930b2cb581d8a85
Leviathan <Leviathan1995@outlook.com>
2015-09-25 07:22:59 +0600

    Update parse.h

--- Soo/parse.h
+++ Soo/parse.h
@@ -10,6 +10,7 @@
 #define __Soo__parse__
 
 #include <iostream>
+#include "global.h"
 #include "ast.h"
 using namespace std;
 
@@ -24,12 +25,15 @@ public:
 	void build_ast();
 	int binary_lsy();
 	int string2int(string str);
+	int getvalue(string name);
 	void parse_number(int number);
 	void parse_defvar();						//define variable
 	void parse_binary(string variable_name);	//binary expreesion
 	void parse_func();	
 	void parse_expreesion();
 	void parse_if();
-	void parse_judge();
+	void parse_while();
+	void parse_judge(exp_type type);
+	void parse_bool(vector<string> codes,string choice);
 };
 #endif /* defined(__Soo__parse__) */
