b698b56f97bc5b2140cde863eb6094889245c699
Leviathan <Leviathan1995@outlook.com>
2015-09-25 07:18:32 +0600

    Update global.cpp

--- Soo/global.cpp
+++ Soo/global.cpp
@@ -7,10 +7,13 @@
 //
 
 #include "global.h"
+#include <string>
 #include <vector>
 #include <string>
 using namespace std;
 
+string node = "soo";
+int nodenum = -1;
 vector<string> codestream;
 vector<symbol> symboltable;
 
@@ -24,11 +27,22 @@ bool symbol_search(string name)
 	return false;
 }
 
-void symbol_insert(string name, int value)
+void symbol_insert_intvalue(string name, int value)
 {
 	symbol newsymbol;
 	newsymbol.symbolname = name;
-	newsymbol.value = value;
+	newsymbol.int_value = value;
+	newsymbol.isarray = false;
+	symboltable.push_back(newsymbol);
+
+}
+
+void symbol_insert_intarray(string name, vector<int> &values)
+{
+	symbol newsymbol;
+	newsymbol.symbolname = name;
+	newsymbol.array_value = values;
+	newsymbol.isarray = true;
 	symboltable.push_back(newsymbol);
 }
 
@@ -37,7 +51,7 @@ int symbol_getvalue(string name)
 	for (int index = 0; index < symboltable.size(); index++)
 	{
 		if (name == symboltable[index].symbolname)
-			return symboltable[index].value;
+			return symboltable[index].int_value;
 	}
 }
 
