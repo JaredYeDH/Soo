70d189ff90a43528ed3a2417ad1fabf66bc61fc3
Leviathan <Leviathan1995@outlook.com>
2015-10-06 11:06:32 +0800

    Update ast.h

--- Soo/ast.h
+++ Soo/ast.h
@@ -16,141 +16,209 @@ using namespace std;
 
 char * string2char(string str);
 string getsign(string choice);
+
+//id
+class id_ast
+{
+public:
+    string idname;
+    id_ast(string name):idname(name){}
+    void codegen();
+    
+};
+
 //immediate number
 class immediate_ast
 {
 public:
     int number;
-	immediate_ast(int num) :number(num){}
-	void codegen();
+    immediate_ast(int num) :number(num){}
+    void codegen();
 };
 
 //number
 class number_ast
 {
 public:
-	int number;
-	number_ast(int num) :number(num){}
-	void codegen();
+    int number;
+    number_ast(int num) :number(num){}
+    void codegen();
 };
 
-//array
-class array_ast
+//string
+class string_ast
 {
 public:
-	string name;
-	vector<int> values;
-	int size;
-	string end;
-	array_ast(string array_name,vector<int> array_values, int array_size) :
-		name(array_name),values(array_values), size(array_size),end("$"){}
-	void codegen();
+    string str;
+    string_ast(string str_value):str(str_value){}
+    void codegen();
 };
 
-//define variable
+//int array
+class intarray_ast
+{
+public:
+    string name;
+    vector<int> values;
+    int size;
+    string end;
+    intarray_ast(string array_name,vector<int> array_values, int array_size) :
+    name(array_name),values(array_values), size(array_size),end("$"){}
+    void codegen();
+};
+
+//string array
+class strarray_ast
+{
+public:
+    string name;
+    vector<string> values;
+    int size;
+    string end;
+    strarray_ast(string array_name,vector<string> array_values,int array_size):
+    name(array_name),values(array_values),size(array_size),end("$"){}
+    void codegen();
+};
+
+//define int variable
 class defvariable_ast
 {
 private:
-	string variable_name;
-	number_ast * variable_value;
+    string int_name;
+    number_ast * int_value;
 public:
-	defvariable_ast(string name,number_ast * number) :
-		variable_name(name),variable_value(number){}
-	void codegen();
+    defvariable_ast(string name,number_ast * number) :
+    int_name(name),int_value(number){}
+    void codegen();
+};
+
+//define string variable
+class defstrvariable_ast
+{
+private:
+    string str_name;
+    string_ast * str_value;
+public:
+    defstrvariable_ast(string name,string_ast *value):
+    str_name(name),str_value(value){}
+    void codegen();
+};
+
+//assign varialbe
+class assign_ast
+{
+private:
+    string variable_name;
+    string value_name;
+public:
+    assign_ast(string name,string value):
+    variable_name(name),value_name(value){}
+    void codegen();
 };
 
 //binary
 class binary_ast
 {
 private:
-	string op;
-	number_ast * left, *right;
+    string op,left, right,save;
 public:
-	binary_ast(string Operator, number_ast *l, number_ast *r) :
-		op(Operator), left(l), right(r){}
-	void codegen();
+    binary_ast(string Operator, string l, string r,string savelabel) :
+    op(Operator), left(l), right(r),save(savelabel){}
+    void codegen();
 };
 
 //judge
 class judge_ast
 {
 private:
-	string choice;
-	string op;
-	immediate_ast * left, *right;
+    string sign;
+    string op;
+    id_ast * left, *right;
 public:
-	judge_ast(string Operator, immediate_ast *l, immediate_ast * r,string sign_choice) :
-		op(Operator), left(l), right(r),choice(sign_choice){}
-	void codegen();
+    judge_ast(string Operator, id_ast *l, id_ast * r,string sign_address) :
+    op(Operator), left(l), right(r),sign(sign_address){}
+    void codegen();
 };
 
-//if 
+//if
 class if_ast
 {
 private:
-	string op;
-	immediate_ast * left, *right;
+    string op;
+    immediate_ast * left, *right;
 public:
-	if_ast(string Operator, immediate_ast *l, immediate_ast * r) :
-		op(Operator), left(l), right(r){}
-	void codegen();
+    if_ast(string Operator, immediate_ast *l, immediate_ast * r) :
+    op(Operator), left(l), right(r){}
+    void codegen();
 };
 
 //while
 class while_ast
 {
 private:
-	string op;
-	immediate_ast *left, *right;
+    string op;
+    immediate_ast *left, *right;
 public:
-	while_ast(string Operator, immediate_ast *l, immediate_ast *r) :
-		op(Operator), left(l), right(r){}
-	void codegen();
+    while_ast(string Operator, immediate_ast *l, immediate_ast *r) :
+    op(Operator), left(l), right(r){}
+    void codegen();
 };
 
 //call funciton
 class callfunc_ast
 {
 private:
-	string func_name;
+    string func_name;
 public:
-	callfunc_ast(string name) :
-		func_name(name){}
-	void codegen();
+    callfunc_ast(string name) :
+    func_name(name){}
+    void codegen();
 };
 
 //function
 class function_ast
 {
 private:
-	string func_name;
+    string func_name;
 public:
-	function_ast(string name) :
-		func_name(name){}
-	void codegen();
+    function_ast(string name) :
+    func_name(name){}
+    void codegen();
 };
 
 //label address
 class sign_ast
 {
 private:
-	string sign;
-	string choice;
+    string choice;
 public:
-	sign_ast(string str) :
-		choice(str){}
-	void get_sign();
-	void codegen();
+    string sign;
+    sign_ast(string str) :
+    choice(str){}
+    void get_sign();
+    void codegen();
 };
 
 //end label
 class end_ast
 {
 private:
-	string end;
+    string end;
+    string sign;
 public:
-	end_ast(string str) :
-		end(str){}
-	void codegen();
+    end_ast(string strend,string sign_address) :
+    end(strend),sign(sign_address){}
+    void codegen();
+};
+
+//system function print()
+class print_ast
+{
+private:
+    string content;
+public:
+    print_ast(string str_content):
+    content(str_content){};
+    void codegen();
 };
 #endif /* defined(__Soo__ast__) */
