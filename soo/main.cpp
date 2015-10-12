32c3bb0875d8a1521ea2b4f1d90aa2264f607c06
Leviathan <Leviathan1995@outlook.com>
2015-10-13 01:26:55 +0800

    Update main.cpp

--- soo/main.cpp
+++ soo/main.cpp
@@ -1,21 +1,23 @@
 //
 //  main.cpp
-//  Soo
+//  Lmachine
 //
-//  Created by leviathan on 15/9/8.
+//  Created by leviathan on 15/9/21.
 //  Copyright (c) 2015年 leviathan. All rights reserved.
 //
 
 #include <iostream>
-#include "token.h"
-#include "parse.h"
+#include "assembler.h"
+#include "lmachine.h"
 using namespace std;
 
-int main() {
-    token tokenmachine;
-  //tokenmachine.tokeninit();
-    tokenmachine.readline();
-    parse parsemachine;
-    parsemachine.build_ast();
+int main() {    
+    Lmachine lvm;
+    lvm.init();         //init lmachine
+    lvm.readline();     //get code
+    Assembler assembler;
+    assembler.assemblerrun();
+    lvm.lvmrun(assembler);
+    cout<<"lvm is over ..."<<endl;
     return 0;
 }
