cff276c7a8f4bd06e4ea5ba51c7629dafc6ea889
Leviathan <Leviathan1995@outlook.com>
2015-10-22 02:25:45 +0800

    Create lvm_assembler.h

---
+++ Lsy_Lvm/lvm/lvm_assembler.h
@@ -0,0 +1,32 @@
+//
+//  assembler.h
+//  Lmachine
+//
+//  Created by leviathan on 15/8/21.
+//  Copyright (c) 2015年 leviathan. All rights reserved.
+//
+
+#pragma once
+#ifndef __Lmachine__assembler__
+#define __Lmachine__assembler__
+
+#include <iostream>
+#include "lvm_global.h"
+using namespace std;
+
+
+class Assembler
+{
+public:
+    Assembler();
+    int searchsymbol(string symbolname,int sign);
+    int count_addrlabel(int index);
+    void assemblerrun();
+    tokentype lexer(string &token,int index);
+    void buildsymbol();             //build symbol table
+    string int2string(int num);
+    int searchcmd(string token);
+    int searchreg(string token);
+    int getopcode(string token);
+};
+#endif /* defined(__Lmachine__assembler__) */
